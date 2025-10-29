cce93ebb14cf494e4a1f699e37ffde34800ee522
tanrobinson <tanrobinson@gmail.com>
2025-10-29 16:47:49 -0400

    Second Module Commit

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -54,8 +54,11 @@ def login_user(request):
 
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)  # Terminate user session
+    data = {"userName": ""}  # Return empty username
+    return JsonResponse(data)
+
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
