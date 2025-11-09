b945a96d6123802a5fb45e313a1979382d4cf904
nathaniel001969 <nathaniel_r@hotmail.com>
2025-11-09 13:10:04 -0500

    Fourht commit

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,9 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Dealers from "./components/Dealers/Dealers";
 import Dealer from "./components/Dealers/Dealer";
 import PostReview from "./components/Dealers/PostReview";
-// import Header from "./components/Header/Header";
-// import Login from "./components/Login/Login";
-// import Register from "./components/Register/Register";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
@@ -14,8 +12,7 @@ function App() {
       <Route path="/dealers" element={<Dealers />} />
       <Route path="/dealer/:id" element={<Dealer />} />
       <Route path="/postreview/:id" element={<PostReview />} />
-      {/* <Route path="/header" element={<Header />} />
-      <Route path="/register" element={<Register />} /> */}
+      <Route path="/Register" element={<Register />} />
     </Routes>
   );
 }
--- server/frontend/src/components/Login/Login.jsx
+++ server/frontend/src/components/Login/Login.jsx
@@ -8,7 +8,7 @@ const Login = ({ onClose }) => {
   const [password, setPassword] = useState("");
   const [open, setOpen] = useState(true);
 
-  let login_url = window.location.origin + "/djangoapp/login/";
+  let login_url = "/djangoapp/login/";
 
   const login = async (e) => {
     e.preventDefault();
