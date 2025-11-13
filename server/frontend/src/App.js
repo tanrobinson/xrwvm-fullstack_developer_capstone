3b11d1f250e0243766ecf287285a9185bb642bd3
tanrobinson <tanrobinson@gmail.com>
2025-11-13 17:38:55 -0500

    React app, nginx.conf cnflict of landing page Home.html

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -4,12 +4,10 @@ import Dealers from "./components/Dealers/Dealers";
 import Dealer from "./components/Dealers/Dealer";
 import PostReview from "./components/Dealers/PostReview";
 import Register from "./components/Register/Register";
-import Home from "./components/Home/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dealers" element={<Dealers />} />
       <Route path="/dealer/:id" element={<Dealer />} />
